6179bc6e5ff4abd52785c3f582eb81226dd6ad56
Ladow1 <vladimir.ksenic@itvkurze.sk>
2020-05-19 20:48:34 +0200

    edi

--- action.php
+++ action.php
@@ -1,8 +1,16 @@
 
 
-<?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
+<?PHP
+echo "Vitaj $meno!";
+// odstránime všetky HTML a PHP značky
+$nazor = strip_tags($nazor);
+// odstránime netlačiteľné znaky zo začiatku a konca reťazca
+$nazor = trim($nazor);
+// prevedieme konce riadkov na značky <BR>
+$nazor = str_replace("\r", "", $nazor);
+$nazor = str_replace("\n", "<BR>", $nazor);
+echo  "Toto je váš názor:
+";
+echo  $nazor;
 ?>
 
