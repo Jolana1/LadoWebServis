14bdaef92bb32939d98a565546b0f0114a8532b8
Jolana1 <ksenicv@icloud.com>
2021-09-26 15:46:41 +0200

    s

--- js/polyfill.js
+++ js/polyfill.js
@@ -14,8 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.*/
 
-"SameSite=None";
-"use strict";
+"SameSite=Lax";
+
 
 {
   let asyncAPIs = [
