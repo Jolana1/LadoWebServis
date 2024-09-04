861b58d2110cdb584db9079d67ddfd430ccf5316
Jolana1 <ksenicv@icloud.com>
2024-09-04 21:07:31 +0200

    refactor: Update product prices and remove unused code in cart.js

--- js/cart.js
+++ js/cart.js
@@ -21,6 +21,7 @@ var modal = document.getElementById("cartModal");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
